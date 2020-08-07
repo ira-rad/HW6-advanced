69856d05b73307f20e2fa778fbadfe704a606866
Ira <irarad1994@gmail.com>
2020-08-07 11:40:49 +0300

    edited 5-th function

--- js/script.js
+++ js/script.js
@@ -28,14 +28,14 @@ const students = [{
 const getStudentsSubjects = (students) =>{
    const subjectsOfStudent = Object.keys(students.subjects);
      return subjectsOfStudent.map(sub => sub.charAt(0).toUpperCase() + sub.slice(1).replace('_', ' ').toLowerCase()  )
-    } 
-    document.writeln(`<p> №1:  ${getStudentsSubjects(students[0])}</p>`)
+    } ;
+    document.writeln(`<p> №1:  ${getStudentsSubjects(students[0])}</p>`);
     //2 
     const getAverageMark = (students) =>{
       const getStudentsMark = Object.values(students.subjects).flat();
       const getStudentsAverageMarks = getStudentsMark.reduce((a, b) => Math.floor(a + b),  0) / getStudentsMark.length;
       return getStudentsAverageMarks.toFixed(2);
-    }
+    };
 
 document.writeln(`<p> №2:  ${getAverageMark(students[0])}</p>`)
    //3
@@ -45,8 +45,8 @@ document.writeln(`<p> №2:  ${getAverageMark(students[0])}</p>`)
     return {name, course, averageMark: getAverageMark(students)}
      
     
-    }
-    console.log(getStudentInfo(students [0]))
+    };
+    console.log(getStudentInfo(students [0]));
 document.writeln(`<p> №3: Результат в console </p>`)
 
 //4
@@ -54,18 +54,16 @@ const getStudentsNames = (students) => students.map((stNames) => stNames.name).s
 document.writeln(`<p> №4:  ${(getStudentsNames(students))}</p>`);
 
 //5
-function getBestStudent(students){
-  const maxMark = Math.max(...students.map(item => getAverageMark(item)));
-  const bestStudent = students.find(item => getAverageMark(item) === maxMark);
-  return bestStudent.name;
-}
-console.log(getBestStudent(students));
+
+  const marksOfStudent = (st) => st.map((elem) => Number(getAverageMark(elem)));
+  getBestStudent = (st) => students[marksOfStudent(st).indexOf(Math.max(...(marksOfStudent(st))))].name;
+document.writeln(`№5: ${(getBestStudent(students))}`)
+
 
 //6
 
 const calculateWordLetters = (word) =>{
   let getResult = {};
-  
     for(const letter of word){
       let letters = letter.toLowerCase();
       getResult[letters] = !getResult[letters] ? 1 : getResult[letters]+1;
